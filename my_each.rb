aada67f07078c25ae257a0d1155e9db329131c22
Steven Chen <ysteven.chen2@gmail.com>
2018-05-04 21:36:54 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(words) # put argument(s) here
   # code here
+  i=0
+
+  while i<words.length
+    words[i]
+    i+=1
+  end
+  
 end
+
+my_each(words[i]) {|x| puts x}
